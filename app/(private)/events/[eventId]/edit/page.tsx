0ac8696b6c5ad312492804904a9a03aa78723858
TeoRomens <matteoroman4@gmail.com>
2025-03-14 10:09:55 +0100

    First Commit

--- app/(private)/events/[eventId]/edit/page.tsx
+++ app/(private)/events/[eventId]/edit/page.tsx
@@ -1,30 +1,30 @@
 import { EventForm } from "@/components/forms/event";
-import { notFound } from "next/navigation"
+import { notFound } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 
-export default async function EditEventPage({
-  params: { eventId },
-}: {
-  params: { eventId: string }
-}) {
+interface EditEventPageProps {
+  params: Promise<{
+    eventId: string
+  }>
+}
+
+export default async function EditEventPage({ params }: EditEventPageProps) {
+  const { eventId } = await params;
+
   const { userId, redirectToSignIn } = await auth()
   if (userId == null) return redirectToSignIn()
 
   const supabase = await createClient()
-  const { data: event } = await supabase
-      .from("events")
-      .select()
-      .eq("id", eventId)
-      .single()
+  const { data: event } = await supabase.from("events").select().eq("id", eventId).single()
 
-  if (event == null) return notFound()
+  if (event == null) notFound()
 
   return (
     <>
       <h1 className="text-[32px] font-semibold mb-3 lg:mb-4 text-primary tracking-tight">Modifica Evento</h1>
       <p className="text-secondary text-[16px] mb-6">The most basic setup for a drawer.</p>
-      <EventForm event={{ ...event, description: event.description || undefined }}/>
+      <EventForm event={{ ...event, description: event?.description || undefined }} />
     </>
   );
 }
